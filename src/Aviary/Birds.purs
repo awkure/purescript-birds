a7600835d18bdb6a7d1321596ec035484dafa024
awkure <awkure@protonmail.ch>
2018-01-04 09:45:35 +0300

    Added composition, minor upd

--- src/Aviary/Birds.purs
+++ src/Aviary/Birds.purs
@@ -7,6 +7,7 @@ import Prelude ( class Semigroupoid
                , id
                , ap
                , const
+               , compose
                , flip )
 
 import Control.Monad ( class Bind
@@ -18,12 +19,38 @@ import Control.Apply ( class Apply
                      , lift2 )
 
 
+infixl 1 t      as &
+infixr 8 cd     as .:
+infixr 8 cdc    as .:.
+infixr 8 cdd    as .::
+infixr 8 cddc   as .::.
+infixr 8 cddd   as .:::
+infixr 8 cdddc  as .:::.
+infixr 8 cdddd  as .::::
+infixr 8 cddddc as .::::.
+
+
+-- | Fixed point Y combinator 
+-- |
+-- | `Λ a . (a → a) → a`
+-- |
+-- | `λ f . (λ x. f (x x)) (λ x . f (x x))`
+fix :: forall a . (a -> a) -> a
+fix f = compose f fix $ f 
+
+
+-- | Reverse application which is 
+-- | probably exist inside `Lens` module
+t :: forall a b . a -> (a -> b) -> b
+t = flip ($)
+
+
 -- | A combinator - applicator
 -- | 
--- | `Λa b . (a → b) → a → b`
+-- | `Λ a b . (a → b) → a → b`
 -- |
 -- | `λ f x . f x`
-applicator ∷ ∀ a b. (a → b) → a → b
+applicator :: ∀ a b. (a -> b) -> a -> b
 applicator = ($)
 
 
@@ -34,8 +61,8 @@ applicator = ($)
 -- | `Λ a b c . (b → c) → (a → b) → a → c`
 -- |
 -- | `λ g f x . g (f x)`
-bluebird ∷ ∀ a b c d. Semigroupoid a ⇒ a c d → a b c → a b d
-bluebird = (<<<)
+bluebird :: ∀ a b c d. Semigroupoid a ⇒ a c d -> a b c -> a b d
+bluebird = compose
 
 
 -- | B' combinator - bluebird prime
@@ -45,8 +72,8 @@ bluebird = (<<<)
 -- | `Λ a b c d . (a → c → d) → a → (b → c) → b → d`
 -- |
 -- | `λ f x g y . f x (g y)`
-bluebird' ∷ ∀ a b c d e. Semigroupoid b ⇒ (a → b d c) → a → b e d → b e c 
-bluebird' = (<<<)(<<<)
+bluebird' :: ∀ a b c d e. Semigroupoid b ⇒ (a -> b d c) -> a -> b e d -> b e c 
+bluebird' = compose compose
 
 
 -- | B1 combinator - blackbird
@@ -56,7 +83,7 @@ bluebird' = (<<<)(<<<)
 -- | `Λ a b c d . (c → d) → (a → b → c) → a → b → d`
 -- |
 -- | `λ f g x y . f (g x y)`
-blackbird ∷ ∀ a b c d e. Semigroupoid e ⇒ e b a → (d → e c b) → d → e c a
+blackbird :: ∀ a b c d e. Semigroupoid e ⇒ e b a -> (d -> e c b) -> d -> e c a
 blackbird = (<<<)<<<(<<<)
 infixr 9 blackbird as ...
 
@@ -68,7 +95,7 @@ infixr 9 blackbird as ...
 -- | `Λ a b c d e . (d → e) → (a → b → c → d) → a → b → c → e`
 -- |
 -- | `λ f g x y z . f (g x y z)`
-bunting ∷ ∀ a b c d e f. Semigroupoid b ⇒ b d c → (f → a → b e d) → f → a → b e c
+bunting :: ∀ a b c d e f. Semigroupoid b ⇒ b d c -> (f -> a -> b e d) -> f -> a -> b e c
 bunting = (<<<)<<<(<<<)<<<(<<<)
 infixr 9 bunting as <.<.<
 
@@ -81,7 +108,7 @@ infixr 9 bunting as <.<.<
 -- |
 -- | `λ f g h x . f (g (h x))`
 -- TODO
-becard ∷ ∀ a b c d. (c → d) → (b → c) → (a → b) → a → d
+becard :: ∀ a b c d. (c -> d) -> (b -> c) -> (a -> b) -> a -> d
 becard = flip (<<<)(<<<) <<< (...)
 
 
@@ -92,7 +119,7 @@ becard = flip (<<<)(<<<) <<< (...)
 -- | `Λ a b c . (a → b → c) → b → a → c`
 -- |
 -- | `λ f x y . f y x`
-cardinal ∷ ∀ a b c. (a → b → c) → b → a → c  
+cardinal :: ∀ a b c. (a -> b -> c) -> b -> a -> c  
 cardinal = flip
 
 
@@ -103,7 +130,7 @@ cardinal = flip
 -- | `Λ a b c d (c → a → d) → (b → c) → a → b → d`
 -- |
 -- | `λ f g x y . f (g y) x`
-cardinal' ∷ ∀ a b c d e. Semigroupoid b ⇒ b d c → (b e c → a) → b e d → a
+cardinal' :: ∀ a b c d e. Semigroupoid b ⇒ b d c -> (b e c -> a) -> b e d -> a
 cardinal' = (>>>)<<<(<<<)
 infixr 9 cardinal' as >..
 
@@ -115,7 +142,7 @@ infixr 9 cardinal' as >..
 -- | `Λ a b c d . (a → c → b → d) → a → b → c → d`
 -- |
 -- | `λ f x y z . f x z y`
-cardinalstar ∷ ∀ a b c d. Semigroupoid a ⇒ a d c → a c b → a d b
+cardinalstar :: ∀ a b c d. Semigroupoid a ⇒ a d c -> a c b -> a d b
 cardinalstar = (>>>)
 
 
@@ -126,7 +153,7 @@ cardinalstar = (>>>)
 -- | `Λ a b c d e . (a → b → d → c → e) → a → b → c → d → e`
 -- |
 -- | `λ f s t u v . f s t v u`
-cardinalstarstar ∷ ∀ a b c d e. Semigroupoid b ⇒ ((b e d → b e c) → a) → b d c → a -- TODO
+cardinalstarstar :: ∀ a b c d e. Semigroupoid b ⇒ ((b e d -> b e c) -> a) -> b d c -> a
 cardinalstarstar = (>>>)(<<<)
 infixr 9 cardinalstarstar as >..<
 
@@ -138,8 +165,8 @@ infixr 9 cardinalstarstar as >..<
 -- | `Λ a b c d . (a → c → d) → a → (b → c) → b → d`
 -- |
 -- | `λ f x g y . f x (g y)`
-dove ∷ ∀ a b c d e. Semigroupoid b ⇒ (a → b d c) → a → b e d → b e c
-dove = (<<<)(<<<) -- ~ bluebird'
+dove :: ∀ a b c d e. Semigroupoid b ⇒ (a -> b d c) -> a -> b e d -> b e c
+dove = compose compose
 infixr 9 dove as <..<
 
 
@@ -150,7 +177,7 @@ infixr 9 dove as <..<
 -- | `Λ a b c d e . (a → b → d → e) → a → b → (c → d) → c → e`
 -- |
 -- | `λ f x y g z . f x y (g z)`
-dickcissel ∷ ∀ a b c d e. Semigroupoid b ⇒ b d c → (a → b e d) → a → b e c
+dickcissel :: ∀ a b c d e. Semigroupoid b ⇒ b d c -> (a -> b e d) -> a -> b e c
 dickcissel = (<<<)(<<<)(<<<)
 infixr 9 dickcissel as <...<
 
@@ -162,7 +189,7 @@ infixr 9 dickcissel as <...<
 -- | `Λ a b c d e . (c → d → e) → (a → c) → a → (b → d) → b → e`
 -- |
 -- | `λ f g x h z . f (g x) (h z)`
-dovekie ∷ ∀ a b c d e f. Semigroupoid c ⇒ c e d → (a → b → c f e) → a → b → c f d
+dovekie :: ∀ a b c d e f. Semigroupoid c ⇒ c e d -> (a -> b -> c f e) -> a -> b -> c f d
 dovekie = (<..<)(...) 
 
 
@@ -173,7 +200,7 @@ dovekie = (<..<)(...)
 -- | `Λ a b c d e . (a → d → e) → a → (b → c → d) → b → c → e`
 -- |
 -- | `λ f x g y z . f x (g y z)`
-eagle ∷ ∀ a b c d e f. Semigroupoid c ⇒ (a → b → c e d) → a → b → c f e → c f d
+eagle :: ∀ a b c d e f. Semigroupoid c ⇒ (a -> b -> c e d) -> a -> b -> c f e -> c f d
 eagle = (...)(<<<)
 
 
@@ -184,7 +211,7 @@ eagle = (...)(<<<)
 -- | `Λ a b c d e f g . (e → f → g) → (a → b → e) → a → b → (c → d → f) → c → d → g`
 -- |
 -- | `λ f g s t h u v . f (g s t) (h u v)`
-baldeagle ∷ ∀ a b c d e f g. Semigroupoid d ⇒ d g c → (f → d b g) → f → (e → d a b) → e → d a c
+baldeagle :: ∀ a b c d e f g. Semigroupoid d ⇒ d g c -> (f -> d b g) -> f -> (e -> d a b) -> e -> d a c
 baldeagle = (...)(<...<)<<<(...)
 
 
@@ -195,7 +222,7 @@ baldeagle = (...)(<...<)<<<(...)
 -- | `Λ a b c . a → b → (b → a → c) → c`
 -- |
 -- | `λ x y f . f y x`
-finch ∷ ∀ a b c. a → (c → a → b) → c → b
+finch :: ∀ a b c. a -> (c -> a -> b) -> c -> b
 finch = flip (flip >>> id)
 
 
@@ -206,7 +233,7 @@ finch = flip (flip >>> id)
 -- | `Λ a b c d . (c → b → a → d) → a → b → c → d`
 -- |
 -- | `λ f x y z . f z y x`
-finchstar ∷ ∀ a b c e d f. Semigroupoid e ⇒ (f → (e b a → e c a) → d) → e c b → f → d
+finchstar :: ∀ a b c e d f. Semigroupoid e ⇒ (f -> (e b a -> e c a) -> d) -> e c b -> f -> d
 finchstar = (>>>)(>>>) <<< flip
 
 
@@ -217,7 +244,7 @@ finchstar = (>>>)(>>>) <<< flip
 -- | `Λ a b c d . (b → c → d) → (a → c) → a → b → d`
 -- |
 -- | `λ f g x y . f y (g x)`
-goldfinch ∷ ∀ a b c d. (b → c → d) → (a → c) → a → b → d 
+goldfinch :: ∀ a b c d. (b -> c -> d) -> (a -> c) -> a -> b -> d 
 goldfinch = (<..<) flip
 
 
@@ -228,8 +255,7 @@ goldfinch = (<..<) flip
 -- | `Λ a b c (a → b → a → c) → a → b → c`
 -- |
 -- | `λ f x y . f x y x`
--- TODO
-hummingbird ∷ ∀ a b m. Bind m ⇒ m a → (a → m b) → m b
+hummingbird :: ∀ a b m. Bind m ⇒ m a -> (a -> m b) -> m b
 hummingbird = (>>=)
 
 
@@ -240,7 +266,7 @@ hummingbird = (>>=)
 -- | `Λ a . a → a` 
 -- |
 -- | `λ x . x`
-idiot ∷ ∀ t a. Category a ⇒ a t t  
+idiot :: ∀ t a. Category a ⇒ a t t  
 idiot = id 
 
 
@@ -251,7 +277,7 @@ idiot = id
 -- | `Λ a b . (a → b) → a → b`
 -- |
 -- | `λ f x . f x`
-idstar ∷ ∀ a b. (a → b) → a → b 
+idstar :: ∀ a b. (a -> b) -> a -> b 
 idstar = id 
 
 
@@ -260,17 +286,17 @@ idstar = id
 -- | `Λ a b c . (a → b → c) → a → b → c`
 -- |
 -- | `λ f x y . f x y`
-idstarstar ∷ ∀ a b c. (a → b → c) → a → b → c 
+idstarstar :: ∀ a b c. (a -> b -> c) -> a -> b -> c 
 idstarstar = id 
 
 
 -- | Psi combinator - psi bird - `on`
 -- | 
--- | `Λ a b . (b -> b -> c) -> (a -> b) -> a -> a -> c`
+-- | `Λ a b . (b → b → c) → (a → b) → a → a → c`
 -- |
 -- | `λ f g . λ x y . f (g x) (g y) `
-psi ∷ ∀ a b c. (b → b → c) → (a → b) → a → a → c
-psi = join <<< ((<<<)(flip <<< (<<<)(<<<))) <<< (<<<)
+on :: ∀ a b c. (b -> b -> c) -> (a -> b) -> a -> a -> c
+on f g = \a b -> g a `f` g b
 
 
 -- | J combinator - Jay
@@ -280,7 +306,7 @@ psi = join <<< ((<<<)(flip <<< (<<<)(<<<))) <<< (<<<)
 -- | `Λ a b c d . (a → b → b) → a → b → a → b`
 -- |
 -- | `λ f x y z . f x (f z y)`
-jay ∷ ∀ a b. (a → b → b) → a → b → a → b
+jay :: ∀ a b. (a -> b -> b) -> a -> b -> a -> b
 jay = lift2 flip ((<<<)(...)) flip
 
 
@@ -289,7 +315,7 @@ jay = lift2 flip ((<<<)(...)) flip
 -- | `Λ a b c . (a → c) → a → b → c`
 -- |
 -- | `λf x y . f x`
-jalt ∷ ∀ a b c d e. Semigroupoid b ⇒ a → b d c → b e d → b e c
+jalt :: ∀ a b c d e. Semigroupoid b ⇒ a -> b d c -> b e d -> b e c
 jalt = const (<<<)
 
 
@@ -298,7 +324,7 @@ jalt = const (<<<)
 -- | `Λ a b c d . (a → b → d) → a → b → c → d`
 -- |
 -- | `λ f x y z . f x y`
-jalt' ∷ ∀ a b c d e f. Semigroupoid e ⇒ a → (f → e c d) → f → e b c → e b d
+jalt' :: ∀ a b c d e f. Semigroupoid e ⇒ a -> (f -> e c d) -> f -> e b c -> e b d
 jalt' = const (<..<)
 
 
@@ -309,7 +335,7 @@ jalt' = const (<..<)
 -- | `Λ a b . a → b → a`
 -- |
 -- | `λ x y . x`
-kestrel ∷ ∀ a b. a → b → a 
+kestrel :: ∀ a b. a -> b -> a 
 kestrel = const 
 
 
@@ -319,27 +345,28 @@ kestrel = const
 -- |
 -- | `Λ a b . a → b → b`
 -- |
--- |`λ x y . y`
-kite ∷ ∀ a b c. Category b ⇒ a → b c c
+-- | `λ x y . y`
+kite :: ∀ a b c. Category b ⇒ a -> b c c
 kite = const id 
 
 
--- | Φcombinator - phoenix 
+-- | Φ combinator - phoenix 
 -- | 
 -- | `Λ a b c d . (b → c → d) → (a → b) → (a → c) → a → d`
 -- |
 -- | `λ f g h x . f (g x) (h x)`
-phoenix ∷ ∀ a b c f. Apply f ⇒ (a → b → c) → f a → f b → f c
+phoenix :: ∀ a b c f. Apply f ⇒ (a -> b -> c) -> f a -> f b -> f c
 phoenix = lift2
 
 
 -- | Q combinator - queer bird
 -- |
 -- | CB
+-- |
 -- | `Λ a b c . (a → b) → (b → c) → a → c`
 -- |
 -- | `λ f g x . g (f x)`
-queer ∷ ∀ a b c d. Semigroupoid a ⇒ a b c → a c d → a b d
+queer :: ∀ a b c d. Semigroupoid a ⇒ a b c -> a c d -> a b d
 queer = (>>>)
 
 
@@ -350,7 +377,7 @@ queer = (>>>)
 -- | `Λ a b c . (b → c) → a → (a → b) → c`
 -- |
 -- | `λ f x g . f (g x)`
-quixotic ∷ ∀ a b c. (b → c) → a → (a → b) → c
+quixotic :: ∀ a b c. (b -> c) -> a -> (a -> b) -> c
 quixotic = (>>>)(flip id) <<< (<<<)
 
 
@@ -361,7 +388,7 @@ quixotic = (>>>)(flip id) <<< (<<<)
 -- | `Λ a b c . a → (b → c) → (a → b) → c`
 -- |
 -- | `λ x f g . f (g x)`
-quizzical ∷ ∀ a b c. a → (b → c) → (a → b) → c
+quizzical :: ∀ a b c. a -> (b -> c) -> (a -> b) -> c
 quizzical = (>>>) <<< flip id
 
 
@@ -372,7 +399,7 @@ quizzical = (>>>) <<< flip id
 -- | `Λ a b c . (a → b) → a → (b → c) → c`
 -- |
 -- | `λ f x g . g (f x)`
-quirky ∷ ∀ a b c. (a → b) → a → (b → c) → c
+quirky :: ∀ a b c. (a -> b) -> a -> (b -> c) -> c
 quirky = (<<<)(flip id)
 
 
@@ -383,7 +410,7 @@ quirky = (<<<)(flip id)
 -- | `Λ a b c . a → (a → b) → (b → c) → c`
 -- |
 -- | `λ x f g . g (f x)`
-quacky ∷ ∀ a b c. c → (c → a) → (a → b) → b
+quacky :: ∀ a b c. c -> (c -> a) -> (a -> b) -> b
 quacky = (<<<)(flip id) <<< flip id
 
 
@@ -394,8 +421,8 @@ quacky = (<<<)(flip id) <<< flip id
 -- | `Λ a b . ((a → b) → a) → (a → b) → b`
 -- |
 -- | `λ x y . y (x y)`
-owl ∷ ∀ a b c d e. Semigroupoid c ⇒ (b → c e d) → b → c a e → c a d
-owl = (<<<)(<<<) -- ap id -- bluebird' 
+owl :: ∀ a b c d e. Semigroupoid c ⇒ (b -> c e d) -> b -> c a e -> c a d
+owl = compose compose 
 infixr 8 owl as .$.
 
 
@@ -406,7 +433,7 @@ infixr 8 owl as .$.
 -- | `Λ a b c . a → (b → a → c) → b → c`
 -- |
 -- | `λ x f y . f y x`
-robin ∷ ∀ a b c. a → (b → a → c) → b → c
+robin :: ∀ a b c. a -> (b -> a -> c) -> b -> c
 robin = flip flip 
 
 
@@ -417,7 +444,7 @@ robin = flip flip
 -- | `Λ a b c d . (b → c → a → d) → a → b → c → d`
 -- |
 -- | `λ f x y z . f y z x`
-robinstar ∷ ∀ a b c d. (b → c → a → d) → a → b → c → d
+robinstar :: ∀ a b c d. (b -> c -> a -> d) -> a -> b -> c -> d
 robinstar = flip <<< (<<<) flip
 
 
@@ -428,7 +455,7 @@ robinstar = flip <<< (<<<) flip
 -- | `Λ a b c d e . (a → c → d → b → e) → a → b → c → d → e`
 -- |
 -- | `λ f s t u v . f s u v t`
-robinstarstar ∷ ∀ a b c d e. (a → c → d → b → e) → a → b → c → d → e
+robinstarstar :: ∀ a b c d e. (a -> c -> d -> b -> e) -> a -> b -> c -> d -> e
 robinstarstar = (<<<) $ flip <<< (<<<) flip
 
 
@@ -439,7 +466,7 @@ robinstarstar = (<<<) $ flip <<< (<<<) flip
 -- | `Λ a b c . (a → b → c) → (a → b) → a → c`
 -- |
 -- | `λ f g x . f x (g x)`
-starling ∷ ∀ m a b. Monad m ⇒ m (a → b) → m a → m b
+starling :: ∀ m a b. Monad m ⇒ m (a -> b) -> m a -> m b
 starling = ap
 
 
@@ -448,7 +475,7 @@ starling = ap
 -- | `Λ a b c d . (b → c → d) → (a → b) → (a → c) → a → d`
 -- |
 -- | `λ f g h x . f (g x) (h x)`
-starling' ∷ ∀ a b c f. Apply f ⇒ (a → b → c) → f a → f b → f c
+starling' :: ∀ a b c f. Apply f ⇒ (a -> b -> c) -> f a -> f b -> f c
 starling' = lift2 
 
 
@@ -459,7 +486,7 @@ starling' = lift2
 -- | `Λ a b . a → (a → b) → b`
 -- |
 -- | `λ x f . f x`
-thrush ∷ ∀ a b. a → (a → b) → b
+thrush :: ∀ a b. a -> (a -> b) -> b
 thrush = flip id 
 
 
@@ -470,7 +497,7 @@ thrush = flip id
 -- | `Λ a b c . a → b → (a → b → c) → c`
 -- |
 -- | `λ x y f . f x y`
-vireo ∷ ∀ a b c. c → b → (c → b → a) → a
+vireo :: ∀ a b c. c -> b -> (c -> b -> a) -> a
 vireo = flip <<< flip id 
 
 
@@ -481,7 +508,7 @@ vireo = flip <<< flip id
 -- | `Λ a b c d . (b → a → b → d) → a → b → b → d`
 -- |
 -- | `λ f x y z . f y x z`
-vireostar ∷ ∀ a b c. (a → b → c) → b → a → c
+vireostar :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 vireostar = flip
 
 
@@ -492,7 +519,7 @@ vireostar = flip
 -- | `Λ a b c d e . (a → c → b → c → e) → a → b → c → c → e`
 -- |
 -- | `λ f s t u v . f s v t u`
-vireostarstar ∷ ∀ a b c d.  (a → c → b → c → d) → a → b → c → c → d
+vireostarstar :: ∀ a b c d.  (a -> c -> b -> c -> d) -> a -> b -> c -> c -> d
 vireostarstar = (<<<) $ ((<<<) flip) <<< flip
 
 
@@ -503,7 +530,7 @@ vireostarstar = (<<<) $ ((<<<) flip) <<< flip
 -- | `Λ a b . (a → a → b) → a → b`
 -- |
 -- | `λ f x . f x x`
-warbler ∷ ∀ a m. Bind m ⇒ m (m a) → m a
+warbler :: ∀ a m. Bind m ⇒ m (m a) -> m a
 warbler = join 
 
 
@@ -514,7 +541,7 @@ warbler = join
 -- | `Λ a b . a → (a → a → b) → b`
 -- |
 -- | `λ x f = f x x`
-worbler ∷ ∀ a b. b → (b → b → a) → a
+worbler :: ∀ a b. b -> (b -> b -> a) -> a
 worbler = flip =<< flip id 
 
 
@@ -525,7 +552,7 @@ worbler = flip =<< flip id
 -- | `Λ a b c . (a → b → b → c) → a → b → c`
 -- |
 -- | `λ f x y . f x y y`
-warblerstar ∷ ∀ a b c. (a → b → b → c) → a → b → c
+warblerstar :: ∀ a b c. (a -> b -> b -> c) -> a -> b -> c
 warblerstar = flip flip id <<< (<<<) ap
 
 
@@ -536,5 +563,73 @@ warblerstar = flip flip id <<< (<<<) ap
 -- | `Λ a b c d . (a → b → c → c → d) → a → b → c → d`
 -- |
 -- | `λ f x y z . f x y z z`
-warblerstarstar ∷ ∀ a b c d. (a → b → c → c → d) → a → b → c → d 
+warblerstarstar :: ∀ a b c d. (a -> b -> c -> c -> d) -> a -> b -> c -> d 
 warblerstarstar = flip flip id <<< (<<<) (flip <<< (<<<) ap)
+
+
+-- | compose2
+cd :: ∀ p a b c d . Semigroupoid p
+   => p a b
+   -> (d -> p c a)
+   -> (d -> p c b)
+cd = dove compose
+
+
+-- | compose3
+cdc :: ∀ p a b c d e . Semigroupoid p
+    => p a b
+    -> (c -> d -> p e a)
+    -> (c -> d -> p e b)
+cdc = dove cd
+
+
+-- | compose4
+cdd :: ∀ p a b c d e f . Semigroupoid p
+    => p e f
+    -> (a -> b -> c -> p d e)
+    -> (a -> b -> c -> p d f)
+cdd = dove cdc
+
+
+-- | compose5
+cddc :: ∀ p a b c d e f g . Semigroupoid p
+     => p a b
+     -> (c -> d -> e -> f -> p g a)
+     -> (c -> d -> e -> f -> p g b)
+cddc = dove cdd
+
+
+-- | compose6
+cddd :: ∀ p a b c d e f g h . Semigroupoid p
+     => p a b
+     -> (c -> d -> e -> f -> g -> p h a)
+     -> (c -> d -> e -> f -> g -> p h b)
+cddd = dove cddc
+
+
+-- | compose7
+cdddc :: ∀ p a b c d e f g h i . Semigroupoid p
+      => p a b
+      -> (c -> d -> e -> f -> g -> h -> p i a)
+      -> (c -> d -> e -> f -> g -> h -> p i b)
+cdddc = dove cddd
+
+
+-- | compose8
+cdddd :: ∀ p a b c d e f g h i j . Semigroupoid p
+      => p a b
+      -> (c -> d -> e -> f -> g -> h -> i -> p j a)
+      -> (c -> d -> e -> f -> g -> h -> i -> p j b)
+cdddd = dove cdddc
+
+
+-- | compose9
+cddddc :: ∀ p a b c d e f g h i j k . Semigroupoid p
+       => p a b
+       -> (c -> d -> e -> f -> g -> h -> i -> j -> p k a)
+       -> (c -> d -> e -> f -> g -> h -> i -> j -> p k b)
+cddddc = dove cdddd 
+
+
+-- cdn 0 = compose 
+-- cdn n = compose dove cdn $ n - 1
